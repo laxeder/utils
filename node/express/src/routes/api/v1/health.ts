ae7433b6c155221d5e38c6ce74922ef8c85b2576
laxeder <laxederdev@gmail.com>
2024-07-28 18:52:13 -0300

    feat(express): Remove test

--- node/express/src/routes/api/v1/health.ts
+++ node/express/src/routes/api/v1/health.ts
@@ -5,7 +5,5 @@ import ResponseHandler from "../../../handlers/ResponseHandler";
 export default function healthRoute(req: Request, res: Response) {
   const date = new Date().toISOString();
 
-  throw new Error("APP0001");
-
   return ResponseHandler.send(res, "APP0001", 200, date);
 }
